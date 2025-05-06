e491633bf4bf632d29e60728882da9ff92c561c4
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-06 15:09:20 +0530

    test

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,6 +1,6 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://41ef-43-224-131-146.ngrok-free.app");
 
 export default socket;
