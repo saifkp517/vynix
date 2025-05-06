6426ff43cbda985bd907c15db2faaafc6a6b7acf
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-06 15:03:51 +0530

    test

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-console.log("Connecting to socket server at:", process.env.DOMAIN_URL);
+console.log("Connecting to socket server at: ", process.env.DOMAIN_URL);
 const socket = io(process.env.DOMAIN_URL);
 
 export default socket;
