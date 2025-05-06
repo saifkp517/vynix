bacb1c5c78a2873826a19a2731c79cbcffc4b832
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-06 15:07:42 +0530

    fuckit

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,6 +2,6 @@
 import { io } from "socket.io-client";
 
 console.log("Connecting to socket server at: ", process.env.DOMAIN_URL);
-const socket = io(process.env.DOMAIN_URL);
+const socket = io("https://41ef-43-224-131-146.ngrok-free.app");
 
 export default socket;
