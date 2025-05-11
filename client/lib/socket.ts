c55ae17e04f94288d3c0be6045cf4a3a9548755c
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-11 22:54:38 +0530

    online

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,6 +1,6 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://074c-43-224-131-146.ngrok-free.app");
 
 export default socket;
