5c93ebf911bea6247d92df04be10619c521307d6
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-19 12:06:08 +0530

    tunnel fix

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,6 +1,8 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("https://upload-delivering-wildlife-cartridge.trycloudflare.com");
+const socket = io("https://upload-delivering-wildlife-cartridge.trycloudflare.com", {
+    withCredentials: true,
+});
 
 export default socket;
