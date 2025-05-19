e8ed6a011ac3a74fb6ed0053c62a471195e081a4
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-19 13:53:07 +0530

    stuff

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io("https://attorneys-hook-education-directions.trycloudflare.com", {
     withCredentials: true,
 });
 
