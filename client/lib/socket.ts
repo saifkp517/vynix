f5e094a28e55cbaf1dfe4d4cdef086a28cd5d36f
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-19 12:18:27 +0530

    new servername

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("https://attorneys-hook-education-directions.trycloudflare.com", {
+const socket = io("https://adapted-retirement-motel-computer.trycloudflare.com/", {
     withCredentials: true,
 });
 
