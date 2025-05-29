bfcb9936034c2767c6444381d142dcd102b940bc
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-29 12:44:24 +0530

    stable + tunnel changes

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,15 +2,15 @@
 import { io } from "socket.io-client";
 
 
-const devSocket = io("http://localhost:4000", {
+// const devSocket = io("http://localhost:4000", {
+//   withCredentials: true,
+// });
+
+
+const prodSocket = io("https://assurance-mixture-oman-production.trycloudflare.com", {
   withCredentials: true,
 });
 
-
-  // const prodSocket = io("https://colin-zone-expense-steps.trycloudflare.com", {
-  //     withCredentials: true,
-  // });
-
-const socket = devSocket;
+const socket = prodSocket;
 
 export default socket;
