0e1c2de71c51ee84e19b4a5bd68051dc555b6102
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-05-30 06:28:13 +0530

    added stuff

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -11,6 +11,6 @@ const prodSocket = io("https://assurance-mixture-oman-production.trycloudflare.c
   withCredentials: true,
 });
 
-const socket = devSocket;
+const socket = prodSocket;
 
 export default socket;
