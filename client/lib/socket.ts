d79f7c45b11bbc7b36ce7e5fce8195e090168e15
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-06-02 15:15:35 +0530

    stable v.2.1

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -7,7 +7,7 @@ import { io } from "socket.io-client";
 // });
 
 
-const socket = io("https://lodging-sanyo-development-anger.trycloudflare.com", {
+const socket = io("https://marine-moore-coat-cms.trycloudflare.com", {
   withCredentials: true,
 });
 
