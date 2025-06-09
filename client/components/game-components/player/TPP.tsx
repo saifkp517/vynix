09c107ab4f2d089f0deb9999fce54ee7cf6b38fb
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-06-09 10:10:36 +0530

    added loading logger, need to make it as a provider

--- client/components/game-components/player/TPP.tsx
+++ client/components/game-components/player/TPP.tsx
@@ -136,7 +136,7 @@ const Player: React.FC<PlayerProps> = ({
     const playerSpeed = useRef(10);
     const playerHeight = 3;
     const gravity = -9.8 * 4;
-    const jumpStrength = 20;
+    const jumpStrength = 30;
 
 
     const raycaster = new THREE.Raycaster();
