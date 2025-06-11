3dcca32cfce4a4c7983900f620b672b688a37cd1
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2025-06-11 14:29:41 +0530

    Update socket.ts

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -7,7 +7,7 @@ import { io } from "socket.io-client";
 // });
 
 
-const socket = io("https://ph-portsmouth-acquisition-organizational.trycloudflare.com", {
+const socket = io("https://casinos-qualifications-background-stud.trycloudflare.com ", {
   withCredentials: true,
 });
 
