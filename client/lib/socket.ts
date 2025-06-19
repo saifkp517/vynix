8a43d710988569e3163132735dc1fb02dc54a2ff
unverifiedchaos5017 <saifkhan501721@gmail.com>
2025-06-19 10:53:31 +0530

    tnl rnm

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,14 +2,14 @@
 import { io } from "socket.io-client";
 
 
-const socket = io("http://localhost:4000", {
-  withCredentials: true,
-});
+//const socket = io("http://localhost:4000", {
+//  withCredentials: true,
+//});
 
 
-// const socket = io("https://synopsis-grant-impressed-children.trycloudflare.com", {
-//   withCredentials: true,
-// });
+ const socket = io("https://kills-salt-beats-cliff.trycloudflare.com", {
+   withCredentials: true,
+ });
 
 
 
